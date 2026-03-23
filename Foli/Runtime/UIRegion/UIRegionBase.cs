db2c3d6747786fa9202552e9b6028f6f0edc8370
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2026-03-23 18:03:22 +0800

    UIRegion

---
+++ Foli/Runtime/UIRegion/UIRegionBase.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+/// <summary> 更新依赖的数据 </summary>
+public abstract class RegionBaseRefreshData { }
+
+/// <summary> 显示依赖的参数 </summary>
+public abstract class RegionBaseShowParam { }
+
+public abstract class UIRegionBase : MonoBehaviour
+{
+    #region 中介
+    private UIRegionHub _hub;
+    public T GetHub<T>() where T : UIRegionHub
+    {
+        return _hub as T;
+    }
+    public void SetHub(UIRegionHub hub)
+    {
+        _hub = hub;
+    }
+    #endregion
+
+    #region 外部调用
+    private bool _isInit;
+    public void Init()
+    {
+        if (_isInit) return;
+        OnInit();
+        _isInit = true;
+    }
+    public void Refresh(RegionBaseRefreshData data)
+    {
+        Init();
+        OnRefresh(data);
+    }
+    public void Show(RegionBaseShowParam param)
+    {
+        if (gameObject.activeSelf) return;
+        gameObject.SetActive(true);
+        OnShow(param);
+    }
+    public void Hide()
+    {
+        if(!gameObject.activeSelf) return;
+        OnHide();
+        gameObject.SetActive(false);
+    }
+    #endregion
+
+    #region 子类重写
+    protected abstract void OnInit();
+    protected abstract void OnRefresh(RegionBaseRefreshData data);
+    protected virtual void OnShow(RegionBaseShowParam param) { }
+    protected virtual void OnHide() { }
+    #endregion
+}
---
+++ Foli/Runtime/UIRegion/UIRegionHub.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRegionHub : MonoBehaviour
+{
+    #region 树状中介
+    private readonly Dictionary<int, UIRegionBase> _regions = new();
+    public void SetRegion(int id, UIRegionBase region)
+    {
+        if (region == null) return;
+        _regions[id] = region;
+        region.SetHub(this);
+    }
+    public T GetRegion<T>(int id) where T : UIRegionBase
+    {
+        if(_regions.TryGetValue(id, out var region))
+            return region as T;
+        return null;
+    }
+    #endregion
+
+    #region 页面返回栈
+    [Flags]
+    public enum Options
+    {
+        None = 0,
+        PushCurrent = 1 << 0,
+        HideOthers = 1 << 1,
+    }
+    private readonly Stack<(int, RegionBaseShowParam)> _stack = new();
+    private int _currentPage = -1;
+    private RegionBaseShowParam _currentParam;
+    
+    public void SetPage(int page, RegionBaseShowParam param = null)
+    {
+        _currentPage = page;
+        _currentParam = param;
+    }
+    public void GoPage(int page, RegionBaseShowParam param = null, Options options = Options.PushCurrent | Options.HideOthers)
+    {
+        if (!_regions.TryGetValue(page, out var region))
+            return;
+        if ((options & Options.HideOthers) != 0)
+        {
+            foreach (var r in _regions.Values)
+                r.Hide();
+        }
+        if ((options & Options.PushCurrent) != 0)
+        {
+            if (_currentPage > 0 && page != _currentPage)
+                _stack.Push((_currentPage, _currentParam));
+        }
+        
+        SetPage(page, param);
+        region.Show(param);
+    }
+    public void GoBack()
+    {
+        if (_stack.Count <= 0) return;
+        var (page, param) = _stack.Pop();
+        GoPage(page, param, Options.HideOthers);
+    }
+    #endregion
+}
